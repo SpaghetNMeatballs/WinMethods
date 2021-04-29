17c0f4e9b79ab3fddfa0d8eb883d843bc901438e
Артемий Андреевич Иванов <ghostsmans@gmail.com>
2021-04-30 00:49:20 +0300

    Добавьте файлы проекта.

---
+++ WinMethods/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace WinMethods
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ProcName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProcID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProcStartTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProcResponding = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(654, 23);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.IndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(654, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(654, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBoxTextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ProcName,
+            this.ProcID,
+            this.ProcStartTime,
+            this.ProcResponding});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 102);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(654, 387);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // ProcName
+            // 
+            this.ProcName.HeaderText = "Имя Процесса";
+            this.ProcName.Name = "ProcName";
+            // 
+            // ProcID
+            // 
+            this.ProcID.HeaderText = "ID процесса";
+            this.ProcID.Name = "ProcID";
+            // 
+            // ProcStartTime
+            // 
+            this.ProcStartTime.HeaderText = "Время старта процесса";
+            this.ProcStartTime.Name = "ProcStartTime";
+            // 
+            // ProcResponding
+            // 
+            this.ProcResponding.HeaderText = "Отвечает ли процесс";
+            this.ProcResponding.Name = "ProcResponding";
+            this.ProcResponding.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 501);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.On_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProcName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProcID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProcStartTime;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn ProcResponding;
+    }
+}
+
---
+++ WinMethods/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace WinMethods
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern int SetWindowText(IntPtr hWnd, string text);
+        [DllImport("user32.dll")]
+        static extern int ShowWindow(IntPtr hWnd, int nCmdShow);
+        [DllImport("user32.dll")]
+        static extern bool IsIconic(IntPtr hWnd);
+
+        int[] id;
+        const int SW_MINIMIZE = 6, SW_SHOWNORMAL = 1;
+        Process curproc;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void On_Load(object sender, EventArgs e)
+        {
+            List<int> idTemp = new List<int>();
+            int currowind = 0;
+
+            foreach (Process process in Process.GetProcesses())
+            {
+                if (!String.IsNullOrEmpty(process.MainWindowTitle))
+                {
+                    comboBox1.Items.Add(String.Format("Процесс: {0} ID: {1} Название окна: {2}\r\n", process.ProcessName, process.Id, process.MainWindowTitle));
+                    idTemp.Add(process.Id);
+                    dataGridView1.Rows.Add();
+                    DataGridViewRow currow = dataGridView1.Rows[currowind];
+                    currow.Cells[0].Value = process.Id;
+                    currow.Cells[1].Value = process.ProcessName;
+                    currow.Cells[2].Value = String.Format("{0}:{1}:{2}", process.StartTime.Hour, process.StartTime.Minute, process.StartTime.Second);
+                    currow.Cells[3].Value = process.Responding;
+                    currowind++;
+                }
+            }
+            id = idTemp.ToArray();
+        }
+
+        private void IndexChanged(object sender, EventArgs e)
+        {
+            curproc = Process.GetProcessById(id[comboBox1.SelectedIndex]);
+        }
+
+        private void TextBoxTextChanged(object sender, EventArgs e)
+        {
+            SetWindowText(curproc.MainWindowHandle, textBox1.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (IsIconic(curproc.MainWindowHandle))
+            {
+                ShowWindow(curproc.MainWindowHandle, SW_SHOWNORMAL);
+            }
+            else
+            {
+                ShowWindow(curproc.MainWindowHandle, SW_MINIMIZE);
+            }
+        }
+    }
+}
